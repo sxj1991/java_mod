7c205d38b309aa135db818cf82fec50dee1dce45
565260908@163.com <565260908@163.com>
2023-07-30 16:06:35 +0800

    k8s HttpUtil api集成

--- httpK8s/src/main/java/com/xjsun/httpk8s/controller/K8sController.java
+++ httpK8s/src/main/java/com/xjsun/httpk8s/controller/K8sController.java
@@ -1,8 +1,16 @@
 package com.xjsun.httpk8s.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.xjsun.httpk8s.openApi.K8sOpenApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class K8sController {
@@ -12,8 +20,10 @@ public class K8sController {
         this.k8sOpenApi = k8sOpenApi;
     }
 
-    @GetMapping("/version")
-    public String getK8sVersion(){
-        return k8sOpenApi.findK8sVersion();
+    @GetMapping("/pod")
+    public Map getK8sVersion(){
+        String k8sVersion = k8sOpenApi.findK8sVersion();
+        Gson gson = new Gson();
+        return gson.fromJson(k8sVersion, HashMap.class);
     }
 }
