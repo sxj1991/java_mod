9d98395fbf5923fa56b1b0770134e9795144481c
孙晓健 <xjsun@travelsky.com.cn>
2024-02-19 16:46:07 +0800

    quartz update

--- quartz/src/main/java/com/lazzy/quartz/custom_quartz/task/QuartzJobExecution.java
+++ quartz/src/main/java/com/lazzy/quartz/custom_quartz/task/QuartzJobExecution.java
@@ -2,13 +2,14 @@ package com.lazzy.quartz.custom_quartz.task;
 
 import com.lazzy.quartz.custom_quartz.entity.SysJob;
 import com.lazzy.quartz.custom_quartz.invoke.JobInvokeUtil;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 
 /**
  * 定时任务处理（允许并发执行）
- *
- *
+ * DisallowConcurrentExecution 阻止定时器并行执行
  */
+@DisallowConcurrentExecution
 public class QuartzJobExecution extends AbstractQuartzJob{
 
     //quartz 会执行该方法
